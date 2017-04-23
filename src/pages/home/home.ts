a7f69365dbc4232c14edb93c8a8eaeab4441ce3c
willyaugusto <willyaugusto@gmail.com>
2017-04-23 16:51:45 -0300

    Aula 2 ok

---
+++ src/pages/home/home.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http'; 
+import { NavController, LoadingController, AlertController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage implements OnInit {
+
+  public carros;
+
+  constructor(
+    public navCtrl: NavController, 
+    private _http: Http,
+    private _loadingCtrl: LoadingController,
+    private _alertCtrl: AlertController) {}
+
+  ngOnInit() {
+    
+    let loader = this._loadingCtrl.create({
+      content: 'Buscando novos carros. Aguarde...'
+    });
+
+    loader.present();
+
+    this._http
+      .get('https://aluracar.herokuapp.com')
+      .map(res => res.json())
+      .toPromise()
+      .then(carros => {
+        this.carros = carros;
+        loader.dismiss();
+      })
+      .catch( err => {
+        console.log(err);
+        loader.dismiss(); 
+        this._alertCtrl.create({
+          title: 'Falha na conexão!',
+          buttons: [{ text: 'Estou ciente' }],
+          subTitle: 'Não foi possível obter a lista de carros. Tente mais tarde.' 
+        }).present();
+      });
+  }
+
+}
